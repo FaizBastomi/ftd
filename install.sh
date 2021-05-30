c9db3387616b3b1a07a1ccc1770f10670ed2c481
FaizBastomi <faizbastomy3@gmail.com>
2021-05-31 03:05:12 +0700

    Fix installation

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ echo "Updating, upgrading and installing needed termux package..."
 pkg update && pkg upgrade -y
 pkg install git nodejs -y
 
+git clone https://github.com/FaizBastomi/termux-fetish-download.git
+cd termux-fetish-download
+
 echo "Now npm will install the needed modules"
 
 npm install
