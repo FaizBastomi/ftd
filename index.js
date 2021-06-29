97d46e9488fcbffa18707eacf61394c0028efcd1
FaizBastomi <faizbastomy3@gmail.com>
2021-06-29 23:48:56 +0700

    prettier

--- index.js
+++ index.js
@@ -1,66 +1,84 @@
-const { random, download, normalize } = require('./utils')
-const readline = require('readline')
-const moment = require('moment-timezone')
-const fs = require('fs')
-const fetch = require('node-fetch')
-const Filetype = require('file-type')
+const { random, download, normalize } = require('./utils');
+const readline = require('readline');
+const moment = require('moment-timezone');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const Filetype = require('file-type');
 
-let time = moment.tz('Asia/Jakarta').format('HH:mm:ss z')
+let time = moment.tz('Asia/Jakarta').format('HH:mm:ss z');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+	input: process.stdin,
+	output: process.stdout,
+});
 
 if (!fs.existsSync('./media')) {
-    console.log('Creating \'media\' folder....')
-    fs.mkdirSync('media')
+	console.log("Creating 'media' folder....");
+	fs.mkdirSync('media');
 }
 
-const start = async function() {
+const start = async function () {
+	console.clear();
+	console.log("Welcome to 'Fetish Downloader'");
+	console.log(
+		'All image was from reddit. For spesific search you must provide a valid subreddit.',
+	);
+	console.log("You can type 'random' for random subreddit.");
+	console.log("type 'exit' to exit");
 
-    console.clear()
-    console.log('Welcome to \'Fetish Downloader\'')
-    console.log('All image was from reddit. For spesific search you must provide a valid subreddit.')
-    console.log('You can type \'random\' for random subreddit.')
-    console.log('type \'exit\' to exit')
+	rl.question('\033[1;36mSearch: \033[1;32m', (input) => {
+		let q = input;
+		if (q === 'exit' || q === 'stop') {
+			console.log('See ya!');
+			process.exit();
+		}
+		rl.question('\033[1;36mHow many: \033[1;32m', async (total) => {
+			await searchImage(q, total).then(() => start());
+		});
+	});
+};
 
-    rl.question('\033[1;36mSearch: \033[1;32m', (input) => {
-        let q = input
-        if (q === 'exit' || q === 'stop') {
-            console.log('See ya!')
-            process.exit()
-        }
-        rl.question('\033[1;36mHow many: \033[1;32m', async (total) => {
-            await searchImage(q, total).then(() => start())
-        })
-    })
-}
+const searchImage = async function (query, total) {
+	let jum = parseInt(total) || 5;
+	let count = 1;
+	if (query === '' || query === 'random' || query === undefined || query === null) {
+		const tag = [
+			'ecchi',
+			'lewdanimegirls',
+			'hentai',
+			'hentaifemdom',
+			'hentaiparadise',
+			'hentai4everyone',
+			'animearmpits',
+			'animefeets',
+			'animethighss',
+			'animebooty',
+			'biganimetiddies',
+			'animebellybutton',
+			'sideoppai',
+			'ahegao',
+		];
+		query = tag[Math.floor(Math.random() * tag.length)];
+	}
+	for (let s = 0; s < jum; s++) {
+		const res = await fetch(
+			`https://meme-api.herokuapp.com/gimme/${encodeURIComponent(query)}`,
+		).catch(console.error);
+		let { url, title } = await res.json();
+		title = normalize(title);
 
-const searchImage = async function(query, total) {
-    let jum = parseInt(total) || 5
-    let count = 1
-    if (query === '' || query === 'random' || query === undefined || query === null) {
-        const tag = ['ecchi', 'lewdanimegirls', 'hentai', 'hentaifemdom', 'hentaiparadise', 'hentai4everyone', 'animearmpits', 'animefeets', 'animethighss', 'animebooty', 'biganimetiddies', 'animebellybutton', 'sideoppai', 'ahegao']
-        query = tag[Math.floor(Math.random() * tag.length)]
-    }
-    for (let s = 0; s < jum; s++) {
-        const res = await fetch(`https://meme-api.herokuapp.com/gimme/${encodeURIComponent(query)}`).catch(console.error)
-        let { url, title } = await res.json()
-        title = normalize(title)
+		const data = await download(url);
+		let { ext } = await Filetype.fromBuffer(data);
 
-        const data = await download(url)
-        let { ext } = await Filetype.fromBuffer(data)
+		if (fs.existsSync(`./media/${title}.${ext}`)) {
+			let name2 = random();
+			console.log(`( ${title} ) name already exist, try with random name... ( ${name2} )`);
+			fs.promises.writeFile(`./media/${name2}.${ext}`, data, { encoding: 'base64' });
+			console.log(`[${time}]`, count++, 'Done');
+		} else {
+			fs.promises.writeFile(`./media/${title}.${ext}`, data, { encoding: 'base64' });
+			console.log(`[${time}]`, count++, 'Done');
+		}
+	}
+};
 
-        if (fs.existsSync(`./media/${title}.${ext}`)) {
-            let name2 = random()
-            console.log(`( ${title} ) name already exist, try with random name... ( ${name2} )`)
-            fs.promises.writeFile(`./media/${name2}.${ext}`, data, { encoding: 'base64' })
-            console.log(`[${time}]`, count++, 'Done')
-        } else {
-            fs.promises.writeFile(`./media/${title}.${ext}`, data, { encoding: 'base64' })
-            console.log(`[${time}]`, count++, 'Done')
-        }
-    }
-}
-
-start()
+start();
--- utils/index.js
+++ utils/index.js
@@ -1,32 +1,32 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 /**
  * Download
  * @param {url} url url want to be downloads
  */
-exports.download = async function(url) {
-    const res = await fetch(url)
-    return res.buffer()
-}
+exports.download = async function (url) {
+	const res = await fetch(url);
+	return res.buffer();
+};
 
 /**
  * random
  * @returns random number
  */
-exports.random = function() {
-    return `${Math.floor(Math.random() * 1e4)}`
-}
+exports.random = function () {
+	return `${Math.floor(Math.random() * 1e4)}`;
+};
 
 /**
- * 
+ *
  * @param {Text} text your text
  * @returns normalized text
  */
-exports.normalize = function(text) {
-    text = text.replace(/[^a-zA-Z0-9 ]/g, ' ').split(' ')
-    let chunk = []
-    for (const i of text) {
-        (i === '') ? '' : chunk.push(i)
-    }
-    return chunk.join(' ')
-}
+exports.normalize = function (text) {
+	text = text.replace(/[^a-zA-Z0-9 ]/g, ' ').split(' ');
+	let chunk = [];
+	for (const i of text) {
+		i === '' ? '' : chunk.push(i);
+	}
+	return chunk.join(' ');
+};
