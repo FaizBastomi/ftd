cca8874ee8e90c4ce0bba6dcbe25e0ee50a5e374
FaizBastomi <faizbastomy3@gmail.com>
2021-07-08 11:10:06 +0700

    Something

--- utils/index.js
+++ utils/index.js
@@ -5,8 +5,8 @@ const fetch = require('node-fetch');
  * @param {url} url url want to be downloads
  */
 exports.download = async function (url) {
-	const res = await fetch(url);
-	return res.buffer();
+    const res = await fetch(url);
+    return res.buffer();
 };
 
 /**
@@ -14,7 +14,7 @@ exports.download = async function (url) {
  * @returns random number
  */
 exports.random = function () {
-	return `${Math.floor(Math.random() * 1e4)}`;
+    return `${Math.floor(Math.random() * 1e4)}`;
 };
 
 /**
@@ -23,10 +23,10 @@ exports.random = function () {
  * @returns normalized text
  */
 exports.normalize = function (text) {
-	text = text.replace(/[^a-zA-Z0-9 ]/g, ' ').split(' ');
-	let chunk = [];
-	for (const i of text) {
-		i === '' ? '' : chunk.push(i);
-	}
-	return chunk.join(' ');
+    text = text.replace(/[^a-zA-Z0-9' ]/g, ' ').split(' ');
+    let chunk = [];
+    for (const i of text) {
+        i === '' ? '' : chunk.push(i);
+    }
+    return chunk.join(' ');
 };
