a0520066be505a1ef1ca0da25bc3b56099968bcc
ファイズ <64179402+FaizBastomi@users.noreply.github.com>
2021-12-01 12:43:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
+# Install Script
+GRN='\033[0;32m'
+CY='\033[0;36m'
+NC='\033[0m'
+
 echo "Updating, upgrading and installing needed termux package..."
 
 pkg update && pkg upgrade -y
 pkg install git nodejs -y
+clear
 
-git clone https://github.com/FaizBastomi/termux-fetish-download.git "ftd"
+echo -e "${GRN}Cloning ${CY}termux-fetish-download${GRN}...${NC}"
+git clone https://github.com/FaizBastomi/termux-fetish-download.git "ftd" --depth 1 --quiet
 cd ftd
 
-echo "Now npm will install the needed modules"
-
-npm install
+echo -e "${GRN}Now npm will install the needed modules${NC}"
+npm install --quiet --no-progress
 rm install.sh
 
-echo "All done. Now type 'cd ftd' then 'npm start' "
+echo "${GRN}All done. Now type ${CY}'cd ftd'${GRN} then ${CY}'npm start'${NC}"
